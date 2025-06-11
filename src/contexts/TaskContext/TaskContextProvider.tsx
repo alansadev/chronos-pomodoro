a3b497bb5cd2442000df43cbd3aedc1b529c11c5
Alan Sa <alkinsret@gmail.com>
2025-06-10 21:05:29 -0300

    load audio and alter index.html

--- src/contexts/TaskContext/TaskContextProvider.tsx
+++ src/contexts/TaskContext/TaskContextProvider.tsx
@@ -1,9 +1,10 @@
-import { useEffect, useReducer } from 'react';
+import { useEffect, useReducer, useRef } from 'react';
 import { initialTaskState } from './initialTaskState';
 import { TaskContext } from './TaskContext';
 import { taskReducer } from './taskReducer';
 import { TimeWorkerManager } from '../../workers/TimeWorkerManager';
 import { TaskActionTypes } from './taskActions';
+import { loadBeep } from '../../utils/loadBeep';
 
 type TaskContextProviderProps = {
 	children: React.ReactNode;
@@ -11,14 +12,18 @@ type TaskContextProviderProps = {
 
 export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	const [state, dispatch] = useReducer(taskReducer, initialTaskState);
+	let playBeepRef = useRef<ReturnType<typeof loadBeep> | null>(null);
 
 	const worker = TimeWorkerManager.getInstance();
 
 	worker.onmessage((e) => {
 		const countDownSeconds = e.data;
-		console.log(countDownSeconds);
 
 		if (countDownSeconds <= 0) {
+			if (playBeepRef.current) {
+				playBeepRef.current();
+				playBeepRef.current = null;
+			}
 			dispatch({
 				type: TaskActionTypes.COMPLETE_TASK,
 			});
@@ -32,13 +37,19 @@ export function TaskContextProvider({ children }: TaskContextProviderProps) {
 	});
 
 	useEffect(() => {
-		// console.log(state);
 		if (!state.activeTask) {
-			console.log('Worker terminado por falta de activeTask');
 			worker.terminate();
 		}
 		worker.postMessage(state);
 	}, [worker, state]);
+
+	useEffect(() => {
+		if (state.activeTask && playBeepRef.current === null) {
+			playBeepRef.current = loadBeep();
+		} else {
+			playBeepRef.current = null;
+		}
+	}, [state.activeTask]);
 
 	return (
 		<TaskContext.Provider value={{ state, dispatch }}>
