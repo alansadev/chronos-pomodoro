bdd0077801f773bc95ff781e915b8e78356bc20f
Alan Sa <alkinsret@gmail.com>
2025-06-12 22:52:11 -0300

    fix: error in toast

--- src/components/Dialog/index.tsx
+++ src/components/Dialog/index.tsx
@@ -1,28 +1,30 @@
 import { ToastContentProps } from 'react-toastify';
-
-import styles from './styles.module.css';
 import { DefaultButton } from '../DefaultButton/index';
 import { ThumbsDownIcon, ThumbsUpIcon } from 'lucide-react';
+
+import styles from './styles.module.css';
 
 export function Dialog({ closeToast, data }: ToastContentProps<string>) {
 	return (
 		<>
-			<div className={styles.container}></div>
-			<p>{data}</p>
-			<div className={styles.buttonsContainer}>
-				<DefaultButton
-					onClick={() => closeToast(true)}
-					icon={<ThumbsUpIcon />}
-					aria-label='Confirmar ação e fechar'
-					title='Confirmar ação e fechar'
-				/>
-				<DefaultButton
-					onClick={() => closeToast(false)}
-					icon={<ThumbsDownIcon />}
-					aria-label='Cancelar ação e fechar'
-					title='Cancelar ação e fechar'
-					color='red'
-				/>
+			<div className={styles.container}>
+				<p>{data}</p>
+
+				<div className={styles.buttonsContainer}>
+					<DefaultButton
+						onClick={() => closeToast(true)}
+						icon={<ThumbsUpIcon />}
+						aria-label='Confirmar ação e fechar'
+						title='Confirmar ação e fechar'
+					/>
+					<DefaultButton
+						onClick={() => closeToast(false)}
+						icon={<ThumbsDownIcon />}
+						aria-label='Cancelar ação e fechar'
+						title='Cancelar ação e fechar'
+						color='red'
+					/>
+				</div>
 			</div>
 		</>
 	);
